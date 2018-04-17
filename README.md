b3e6710edb38a5e456a2f64558239d783eeb5e74
Karl Kirsch <21990134+coderkarl@users.noreply.github.com>
2018-04-16 22:33:23 -0500

    Initial Navigation Simulation

---
+++ README.md
@@ -0,0 +1,47 @@
+# Simple ROS simulation for path planning with lidar
+clone or download zip to catkin_ws/src/
+`git clone https://github.com/CentralIllinoisRoboticsClub/nav_sim.git`
+
+## Dependencies
+```
+cd ~/catkin_ws/src/
+git clone https://github.com/josephduchesne/light_scan_sim.git
+cd ~/catkin_ws/
+catkin_make
+```
+
+## Usage
+`roslaunch nav_sim nav_sim.launch`
+(A roscore master will automatically start for your first launch file)
+
+`roslaunch nav_sim move_base_nav.launch`
+
+If ROS cannot find the .py scripts, try changing the file permissions:
+`chmod +x /path/to/filename.py`
+
+Use the 2D Nav Goal button at the top of RVIZ. Click and drag to specify goal.
+Turn on/off Map, local_costmap, global_costmap as desired.
+NOTE, you cannot specify a goal outside of global_costmap.
+Increase global_costmap size if desired.
+Try a non-static global_costmap.
+
+## Tuning
+Start with trying to understand the effects of the following files:
+base_local_planner.yaml (teb_local_planner is another option to try later)
+costmap_common_params.yaml
+local_costmap_params.yaml
+global_costmap_params.yaml
+
+These files are used by move_base_nav.launch, which can be restarted separately while leaving your simulation state as is.
+
+If the robot gets stuck, which is likely to happen so far, restart both launch files.
+
+## Limited turning radius model
+Currently, scripts/sim_bot.py has the function sim_cmd_callback(self,data)
+This function modifies the linear velocity and angular velocity commands from the navigation planning.
+It ensures the maximum curvature (2.0 1/meters for wheele) is not exceeded.
+However, this is one reason the sim bot gets stuck easily.
+The navigation move_base node thinks the bot can spin in place.
+We either need to increase the robot and/or costmap inflation radius, or find a local planner that does a better job of using reverse when needed.
+The teb_local_planner (see move_base_nav.launch) can be used instead of base_local_planner.
+teb_local_planner does allow reverse. It supposedly controls minimum turning radius, but I could not get it to work.
---
+++ scripts/sim_bot.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python
+
+import rospy, math
+import numpy as np
+from wheele_msgs.msg import SpeedCurve
+
+from std_msgs.msg import Int16
+
+from nav_msgs.msg import Odometry
+import tf
+from geometry_msgs.msg import Twist, Quaternion, Point, Pose, Vector3
+
+from sensor_msgs.msg import LaserScan
+
+import time
+import sys
+
+class SimBot():
+    def __init__(self):
+        rospy.init_node('bot_simulator')
+        
+        rospy.Subscriber('cmd_vel', Twist, self.sim_cmd_callback, queue_size=1)
+        
+        self.USE_SIMPLE_SCAN_SIM = False
+        if(self.USE_SIMPLE_SCAN_SIM):
+            self.scan_pub = rospy.Publisher('scan', LaserScan, queue_size=50)
+        
+        self.odom_pub = rospy.Publisher('odom', Odometry, queue_size=5)
+        self.odom_broadcaster = tf.TransformBroadcaster()
+        self.prev_time = rospy.Time.now()
+        
+        print('Initializing bot simulator.')
+        
+        self.v = 0
+        self.w = 0
+        self.v_cmd = 0
+        self.w_cmd = 0
+        
+        self.max_accel= 5
+        self.max_w_dot = 3
+        
+        self.bot_rad = 0
+        self.botx = 0
+        self.boty = 0    
+                
+    def sim_cmd_callback(self, data):
+        v = data.linear.x
+        w = data.angular.z
+        
+        # Turnig Radius limit = 0.5 meters
+        MAX_CURVATURE = 2.0
+        if(abs(v) < 0.1 and abs(w) > 0.0):
+            v = 0.5*np.sign(v)
+            if(v == 0.0):
+                v = 0.5
+        elif(abs(v) > 0):
+            curv = w/v
+            if(abs(curv) > MAX_CURVATURE):
+                w = np.sign(curv)*v*MAX_CURVATURE
+            
+        self.v_cmd = v
+        self.w_cmd = w
+    
+    def update_odom(self):
+        
+        t2 = rospy.Time.now()
+        t1 = self.prev_time
+        self.prev_time = t2
+        dt = (t2-t1).to_sec()
+        
+        accel = (self.v_cmd - self.v)/dt
+        mu_v = np.sign(accel)
+        if(abs(accel) > self.max_accel):
+            accel = self.max_accel*mu_v
+        
+        dmeters = self.v*dt + 0.5*accel*dt**2
+        self.v += accel*dt
+        
+        w_dot = (self.w_cmd - self.w)/dt
+        mu_w = np.sign(w_dot)
+        if(abs(w_dot) > self.max_w_dot):
+            w_dot = self.max_w_dot*mu_w
+        
+        dtheta = self.w*dt + 0.5*w_dot*dt**2
+        self.w += w_dot*dt
+
+        #update bot position
+        self.bot_rad = self.bot_rad + dtheta
+        dx = dmeters*np.cos(self.bot_rad)
+        dy = dmeters*np.sin(self.bot_rad)
+        self.botx = self.botx + dx
+        self.boty = self.boty + dy
+        
+        odom_quat = tf.transformations.quaternion_from_euler(0, 0, self.bot_rad)
+        self.odom_broadcaster.sendTransform(
+        (self.botx, self.boty, 0.),
+        odom_quat,
+        t2,
+        "base_link",
+        "odom"
+        )
+        
+        odom = Odometry()
+        odom.header.stamp = t2
+        odom.header.frame_id = "odom"
+
+        # set the position
+        odom.pose.pose = Pose(Point(self.botx, self.boty, 0.), Quaternion(*odom_quat))
+
+        # set the velocity
+        odom.child_frame_id = "base_link"
+        odom.twist.twist = Twist(Vector3(self.v, 0, 0), Vector3(0, 0, self.w))
+
+        # publish the message
+        self.odom_pub.publish(odom)
+        
+        # LaserScan simulation
+        # initially just check point obstacle at 10,0 in odom
+        if(self.USE_SIMPLE_SCAN_SIM):
+            scan = LaserScan()
+            scan.header.frame_id = 'laser'
+            scan.range_min = 0.2
+            scan.range_max = 30.0
+            fov = 48
+            nDet = 16
+            
+            current_time = rospy.Time.now()
+            scan.header.stamp = current_time
+            scan.angle_min = -fov/2*3.14/180.0
+            scan.angle_max = fov/2*3.14/180.0
+            scan.angle_increment = (fov/nDet)*3.14/180.0
+            scan.time_increment = 0.001
+            scan.ranges = []
+            
+            obstacle_found = False
+            dx = 10.0 - self.botx
+            dy = 0.0 - self.boty
+            theta = math.atan2(dy,dx)
+            if(abs(self.bot_rad - theta) < fov/2*3.14/180.0):
+                dist = math.sqrt(dx**2+dy**2)
+                if(dist < 30.0):
+                    obstacle_found = True
+                    for sub_theta_deg in range(-fov/2,fov/2+1,fov/nDet):
+                        if(abs(self.bot_rad+sub_theta_deg*3.14/180.0 - theta) < scan.angle_increment):
+                            scan.ranges.append(dist)
+                        else:
+                            scan.ranges.append(50.0)
+            if not obstacle_found:
+                for k in range(nDet):
+                    scan.ranges.append(50.0)
+            self.scan_pub.publish(scan)
+        
+
+if __name__ == '__main__':
+    try:
+        sim_bot = SimBot()
+        print("Starting bot simulator")
+
+        r = rospy.Rate(50.0)
+        while not rospy.is_shutdown():
+            sim_bot.update_odom()
+            r.sleep()
+            
+    except rospy.ROSInterruptException:
+        pass
