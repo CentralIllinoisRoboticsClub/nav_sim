942535e3b6eca88d3f1cb19b4e86f657c583755c
Karl Kirsch <21990134+coderkarl@users.noreply.github.com>
2018-07-28 17:15:37 -0500

    min_obs_elev, sim noise readings with a pitched scan

--- scripts/sim_bot.py
+++ scripts/sim_bot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import rospy, math
+import rospy, math, random
 import numpy as np
 
 from std_msgs.msg import Int16
@@ -23,7 +23,7 @@ class SimBot():
         self.USE_SIMPLE_SCAN_SIM = True
         
         if(self.USE_SIMPLE_SCAN_SIM):
-            self.scan_pub = rospy.Publisher('scan2', LaserScan, queue_size=50)
+            self.scan_pub = rospy.Publisher('noise_scan', LaserScan, queue_size=50)
         
         self.odom_pub = rospy.Publisher('odom', Odometry, queue_size=5)
         self.odom_broadcaster = tf.TransformBroadcaster()
@@ -43,6 +43,11 @@ class SimBot():
         self.bot_rad = 0
         self.botx = 0
         self.boty = 0    
+        
+        self.noise_scan_active = False
+        self.active_duration = 0.0
+        self.check_time = rospy.Time.now()
+        self.noise_scan_start_time = rospy.Time.now()
                 
     def sim_cmd_callback(self, data):
         v = data.linear.x
@@ -124,18 +129,20 @@ class SimBot():
         self.odom_pub.publish(odom)
         
         # LaserScan simulation
-        # initially just check point obstacle at 10,0 in odom
+        # initially just check point obstacle at 5,0 in odom
         if(self.USE_SIMPLE_SCAN_SIM):
             #if( (t2 - self.scan_time1).to_sec() > 30.0):
             #    self.USE_SIMPLE_SCAN_SIM = False
             scan = LaserScan()
-            scan.header.frame_id = 'laser'
+            scan.header.frame_id = 'noise_laser'
             scan.range_min = 0.2
-            scan.range_max = 30.0
-            fov = 48
-            nDet = 16
+            scan.range_max = 50.0
+            fov = 24
+            nDet = 8
             
             current_time = rospy.Time.now()
+            delta_check_time = (current_time - self.check_time).to_sec()
+            delta_active_time = (current_time - self.noise_scan_start_time).to_sec()
             scan.header.stamp = current_time
             scan.angle_min = -fov/2*3.14/180.0
             scan.angle_max = fov/2*3.14/180.0
@@ -143,25 +150,56 @@ class SimBot():
             scan.time_increment = 0.001
             scan.ranges = []
             
-            obstacle_found = False
-            dx = 5.0 - self.botx
-            if( (t2 - self.scan_time1).to_sec() > 15.0):
-                dx = 2.0 - self.botx
-            dy = 0.0 - self.boty
-            theta = math.atan2(dy,dx)
-            if(abs(self.bot_rad - theta) < fov/2*3.14/180.0):
-                dist = math.sqrt(dx**2+dy**2)
-                if(dist < 30.0):
-                    obstacle_found = True
-                    for sub_theta_deg in range(-fov/2,fov/2+1,fov/nDet):
-                        if(abs(self.bot_rad+sub_theta_deg*3.14/180.0 - theta) < scan.angle_increment):
-                            scan.ranges.append(dist)
-                        else:
-                            scan.ranges.append(50.0)
-            if not obstacle_found:
+            # random times we see noise obstacle at 5,0 (if in fov)
+            #   The ground arc is also added at the same random time for now.
+            #   This state is latched for 1-2 sec
+            if(not self.noise_scan_active and delta_check_time > 1.0): #only check the random number every second
+                rn  = random.randint(0,10)
+                rospy.loginfo('checking random number: %d', rn)
+                if(rn > 6):
+                    self.noise_scan_active = True
+                    self.noise_scan_start_time = current_time
+                    self.active_duration = random.random()*2.0 + 1.0
+                    rospy.loginfo('active duration: %0.1f',self.active_duration)
+                    
+                #end if
+                self.check_time = current_time
+            else:
+                if( delta_active_time > self.active_duration ):
+                    self.noise_scan_active = False
+                #end if
+            #end if
+            
+            if(self.noise_scan_active):
+                obstacle_found = False
+                dx = 5.0 - self.botx
+                if( (t2 - self.scan_time1).to_sec() > 15.0):
+                    dx = 2.0 - self.botx
+                dy = 0.0 - self.boty
+                theta = math.atan2(dy,dx)
+                if(abs(self.bot_rad - theta) < fov/2*3.14/180.0):
+                    dist = math.sqrt(dx**2+dy**2)
+                    if(dist < 30.0):
+                        obstacle_found = True
+                        for sub_theta_deg in range(-fov/2,fov/2+1,fov/nDet):
+                            if(abs(self.bot_rad+sub_theta_deg*3.14/180.0 - theta) < scan.angle_increment):
+                                scan.ranges.append(dist)
+                            else:
+                                scan.ranges.append(6.0)
+                if not obstacle_found:
+                    for k in range(nDet):
+                        scan.ranges.append(6.0)
+            else:
                 for k in range(nDet):
-                    scan.ranges.append(50.0)
+                    scan.ranges.append(0.0)
+            #end if active or not
             self.scan_pub.publish(scan)
+            
+            br = tf.TransformBroadcaster()
+            roll = 0
+            pitch = 0.08
+            laser_quat = tf.transformations.quaternion_from_euler(roll, pitch, 0)
+            br.sendTransform((0,0,0.3),laser_quat,t2,"noise_laser","base_link")
         
 
 if __name__ == '__main__':
